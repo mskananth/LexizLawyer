49cee689b603f10186a859748e85736279b1f0e5
PSA24 <react.ananth@gmail.com>
2026-07-20 19:28:17 +0530

    Refactor Login tests and enhance input validation checks

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,14 +1,56 @@
+require("dotenv").config();
+console.log(process.env.BASE_URL);
+
 const { test, expect } = require("@playwright/test");
 const { getOTP } = require("../utils/emailHelper");
-<<<<<<< HEAD
-// const { regOTP } = require("../utils/emailHelper1");
-=======
-const { getOTP1 } = require("../utils/emailHelper1");
->>>>>>> 3b629d05752e419b285eafa09a78944e0414f134
+const { regOTP } = require("../utils/emailHelper1");
 const { extractOTP } = require("../utils/otpExtractor");
-require("dotenv").config();
 
-test("Email OTP Login", async ({ page }) => {
+test("Verify UI Elements", async ({ page }) => {
+  await page.goto(process.env.BASE_URL);
+  await expect(page.locator("img.lx-logo")).toBeVisible();
+  await expect(page.locator("#id_otp_identifier")).toBeVisible();
+  await expect(page.getByRole("button", { name: "Send OTP" }));
+});
+
+test("Empty Input Validations", async ({ page }) => {
+  await page.goto(process.env.BASE_URL);
+  const sendOtpButton = page.getByRole("button", { name: "Send OTP" });
+  await sendOtpButton.click();
+  await expect(page.getByText("Email or Mobile is required")).toBeVisible();
+});
+
+test("Invalid Email Format", async ({ page }) => {
+  await page.goto(process.env.BASE_URL);
+  await page.locator("#id_otp_identifier").fill("agile.ananth@");
+
+  const sendOtpButton = page.getByRole("button", { name: "Send OTP" });
+  await sendOtpButton.click();
+  await expect(
+    page.getByText("Enter a valid email or 10-digit mobile number"),
+  ).toBeVisible();
+});
+test("Invalid Mobile Format", async ({ page }) => {
+  await page.goto(process.env.BASE_URL);
+  await page.locator("#id_otp_identifier").fill("98431100");
+
+  const sendOtpButton = page.getByRole("button", { name: "Send OTP" });
+  await sendOtpButton.click();
+  await expect(
+    page.getByText("Enter a valid email or 10-digit mobile number"),
+  ).toBeVisible();
+});
+
+test("Login with Unregistered Email", async ({ page }) => {
+  await page.goto(process.env.BASE_URL);
+  await page.locator("#id_otp_identifier").fill("agile.ananth1@gmail.com");
+
+  const sendOtpButton = page.getByRole("button", { name: "Send OTP" });
+  await sendOtpButton.click();
+  await expect(page.getByText("User not found")).toBeVisible();
+});
+
+test("Valid Regsitered Email", async ({ page }) => {
   await page.goto(process.env.BASE_URL);
 
   await page.locator("#id_otp_identifier").fill(process.env.EMAIL);
@@ -36,11 +78,6 @@ test("Email OTP Login", async ({ page }) => {
   await page.getByRole("button", { name: "Verify OTP" }).click();
   await page.waitForLoadState("networkidle");
   await expect(page).toHaveTitle("LexiZ Lawyers");
-<<<<<<< HEAD
-  await page.pause();
-=======
-  // await page.pause();
->>>>>>> 3b629d05752e419b285eafa09a78944e0414f134
 });
 
 // test("Signup with OTP ", async ({ page }) => {
